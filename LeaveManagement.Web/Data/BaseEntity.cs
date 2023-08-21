176e58906d9151058b131f4eca751ed96b1751ce
Stephen Hollingsworth <633706+Merganser2@users.noreply.github.com>
2023-08-21 16:45:48 -0700

    Update Views with Employee User

--- LeaveManagement.Web/Data/BaseEntity.cs
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -1,8 +1,11 @@
 ﻿namespace LeaveManagement.Web.Data
 {
-    public partial class BaseEntity
+    // Note that this is an abstract class, cannot be instantiated on its own
+    public abstract class BaseEntity
     {
-        public int Id { get; set; } // Entity Framework will automatically make this an auto-incrementing(?) PK
+        // Entity Framework will automatically make a property of type int named 'Id' OR '<TableName>Id'
+        // a PK that is auto-incrementing ("SqlServer:Identity" for SQL Server). TODO: Try with postgres to see if does SERIAL
+        public int Id { get; set; }  
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
     }
